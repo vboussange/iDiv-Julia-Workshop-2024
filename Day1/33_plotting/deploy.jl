40f91f87317233c0549084a69afd4e78de2c17d0
Victor Boussange <vic.boussange@gmail.com>
2024-08-20 19:47:01 +0200

    updated plots tutorial

--- Day1/33_plotting/deploy.jl
+++
@@ -1,7 +0,0 @@
-using Literate
-
-Literate.notebook("33_plotting.jl",
-                  credit=false, execute=true, mdstrings=true)
-
-Literate.markdown("33_plotting.jl",
-                  credit=false, execute=true, mdstrings=true, flavor=Literate.CommonMarkFlavor())
